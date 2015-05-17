99ddee2ced1969bcbadbb1e5a4ba96949149af6f
Kidel <tano.design@gmail.com>
2015-05-17 12:02:32 +0200

    javascript+css per file input stile bootstrap.

--- WebContent/js/custom.jquery.js
+++ WebContent/js/custom.jquery.js
@@ -39,6 +39,13 @@ $(".price").priceFormat({
 	centsLimit: 2
 });
 
+$(document).on('change', '.btn-file :file', function() {
+  var input = $(this),
+      numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+});
+
 $(document).ready(function() {
 	$(".indexDesc").dotdotdot({
 		ellipsis: 	'... ',
@@ -48,4 +55,17 @@ $(document).ready(function() {
 		ellipsis: 	'... ',
 		watch: 		'window'
 	});
+	
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } /*else {
+            if( log ) alert(log);
+        }*/
+        
+    });
 });

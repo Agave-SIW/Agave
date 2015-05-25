7bb407da50c3ed926d987160ee871f50b7e08fcb
Kidel <tano.design@gmail.com>
2015-05-25 13:08:35 +0200

    Controlli su file upload (dimensione, formato) e tooltip.

--- src/it/uniroma3/helper/ContextHelper.java
+++ src/it/uniroma3/helper/ContextHelper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
 /**
  * Helper class for Faces Context management
@@ -17,11 +18,13 @@ public class ContextHelper {
 
 	private FacesContext currentInstance;
 	private ExternalContext externalContext;
+	private ServletContext servletContext;
 	private Map<String, Object> sessionMap;
 
 	public ContextHelper() {
 		this.currentInstance = FacesContext.getCurrentInstance();
 		this.externalContext = currentInstance.getExternalContext();
+		this.servletContext = (ServletContext) externalContext.getContext();
 		this.sessionMap = externalContext.getSessionMap();
 	}
 	
@@ -70,6 +73,14 @@ public class ContextHelper {
 
 	public void setSessionMap(Map<String, Object> sessionMap) {
 		this.sessionMap = sessionMap;
+	}
+
+	public ServletContext getServletContext() {
+		return servletContext;
+	}
+
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
 	}
 
 }
